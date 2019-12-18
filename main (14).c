bd62af0e2d739cdb846f79490fcd79000ba2ed89
divya413 <54680782+divya413@users.noreply.github.com>
2019-12-18 12:09:07 +0530

    Add files via upload

---
+++ main (14).c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%4==0)
+    {
+        printf("leap year");
+        
+    }
+else
+printf("not leap year");
+return 0;
+}
+
+ 
