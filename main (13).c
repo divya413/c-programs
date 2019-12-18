4b7a9cc151cb958605e0f5ac1cc96feb05663ace
divya413 <54680782+divya413@users.noreply.github.com>
2019-12-18 12:02:33 +0530

    Add files via upload

---
+++ main (13).c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a%5==0&&a%11==0)
+{
+    printf("a is divisible");
+    
+}
+else
+printf("a is not divisible");
+return 0;
+}
